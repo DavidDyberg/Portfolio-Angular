fffc041d2f217dc7580dcf78ea0c6c9f4482c467
David Dyberg <david.dyberg@gmail.com>
2025-05-05 11:14:01 +0200

    feat: setup routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 @Component({
   selector: 'app-root',
