6f1f5efb5ae111a3379dbbcc54279b694c3c9631
David Dyberg <david.dyberg@gmail.com>
2025-05-11 18:47:40 +0200

    feat: cache about data

--- src/app/services/about.service.ts
+++ src/app/services/about.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 
 type AboutType = {
   firstName?: string;
@@ -22,10 +22,19 @@ type AboutType = {
 })
 export class AboutService {
   private apiUrl = 'https://david-dyberg-portfolio-api.vercel.app/api/about';
+  private cachedData: AboutType | null = null;
 
   constructor(private http: HttpClient) {}
 
   getAboutData(): Observable<AboutType> {
-    return this.http.get<AboutType[]>(this.apiUrl).pipe(map((data) => data[0]));
+    if (this.cachedData) {
+      return of(this.cachedData);
+    }
+    return this.http.get<AboutType[]>(this.apiUrl).pipe(
+      map((data) => {
+        this.cachedData = data[0];
+        return this.cachedData;
+      })
+    );
   }
 }
