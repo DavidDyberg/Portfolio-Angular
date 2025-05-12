ebe6afad22db90cc057680f8e9d37c48b0d48e92
David Dyberg <david.dyberg@gmail.com>
2025-05-12 19:22:54 +0200

    feat: fetch projects data

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -1,8 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from '../../services/projects.service';
+
+type projectType = {
+  _id: string;
+  title: string;
+  description: string;
+  image?: string;
+  techStack?: string[];
+  githubLink?: string;
+  liveDemo?: string;
+};
 
 @Component({
   selector: 'app-project',
   imports: [],
-  template: ` <h1 class="text-amber-50">My project component</h1>`,
+  template: `
+    <h1 class="text-amber-50">My project component</h1>
+    @for (project of projects; track project._id) {
+    <div>
+      <h2 class="text-amber-50">{{ project.title }}</h2>
+    </div>
+    } @empty {
+    <p>There are no projects at the moment</p>
+    } @if (loading) {
+    <p>Loading...</p>
+    }
+  `,
 })
-export class ProjectComponent {}
+export class ProjectComponent implements OnInit {
+  projects: projectType[] = [];
+  loading: boolean = false;
+
+  constructor(private projectsService: ProjectsService) {}
+
+  ngOnInit(): void {
+    this.loadProjects();
+  }
+
+  loadProjects(): void {
+    this.loading = true;
+    this.projectsService.getProjects().subscribe({
+      next: (data) => {
+        this.projects = data;
+        console.log('Projects loaded:', this.projects);
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching projects:', error);
+        this.loading = false;
+      },
+    });
+  }
+}
--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+type projectType = {
+  _id: string;
+  title: string;
+  description: string;
+  image?: string;
+  techStack?: string[];
+  githubLink?: string;
+  liveDemo?: string;
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectsService {
+  private apiUrl = 'https://david-dyberg-portfolio-api.vercel.app/api/projects';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProjects(): Observable<projectType[]> {
+    return this.http.get<projectType[]>(this.apiUrl);
+  }
 }
